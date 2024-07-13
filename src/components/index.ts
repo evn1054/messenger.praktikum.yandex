91d00fded5695a9bc7c677ea78c567078666e049
Nagaev Evgeny <evn1054@gmail.com>
2024-07-13 13:58:51 +0300

    sprint_1 init project

---
+++ src/components/index.ts
@@ -0,0 +1,18 @@
+export { Button } from './button';
+export { Input } from './input';
+export { FormWrapper } from './formWrapper';
+export { ErrorPageComponent } from './errorPageComponent';
+export { SearchInput } from './searchInput';
+export { Dialog } from './dialog';
+export { Avatar } from './avatar';
+export { ChatHeader } from './chatHeader';
+export { ChatFooter } from './chatFooter';
+export { ChatMain } from './chatMain';
+export { BurgerMenu } from './burgerMenu';
+export { Message } from './message';
+export { ProfileAvatar } from './profileAvatar';
+export { UserName } from './userName';
+export { ProfileMainInfo } from './profileMainInfo';
+export { ProfileMainInfoList } from './profileMainInfoList';
+export { IconButton } from './iconButton';
+
---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export {LoginPage} from './login';
+export {RegistrationPage} from './registration';
+export {PageNotFound} from './errorPages/pageNotFound';
+export {ServerError} from './errorPages/serverError';
+export {ChatList} from './chatList';
+export {NavigatePage} from './navigate';
+export {ProfilePage} from './profile';

412c331c25a52aad0cf8099871c81ebecf3e817b
Nagaev Evgeny <evn1054@gmail.com>
2024-07-21 17:17:04 +0300

    sprint_2 fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
-import * as path from "node:path";
+import { defineConfig } from 'vite';
+import * as path from 'node:path';
 
 export default defineConfig({
-    root: '.',
-    build: {
-        outDir: 'dist'
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@styles': path.resolve(__dirname, './src/styles'),
     },
-    resolve: {
-        alias: {
-            '@components': path.resolve(__dirname, './src/components'),
-            '@assets': path.resolve(__dirname, './src/assets'),
-            '@styles': path.resolve(__dirname, './src/styles')
-        }
+  },
+  server: {
+    port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/variables.scss";',
+      },
     },
-    server: {
-        port: 3000
-    },
-})
+  },
+});

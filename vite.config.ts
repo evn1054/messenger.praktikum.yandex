4c916520fb151da9ed5e5d49f308660645c703a0
Nagaev Evgeny <evn1054@gmail.com>
2024-07-21 17:23:57 +0300

    sprint_2 fix css files to scss

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@import "@/styles/variables.scss";',
+        additionalData: '@import "src/styles/variables.scss";',
       },
     },
   },

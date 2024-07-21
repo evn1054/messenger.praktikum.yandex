bacfbd70dd084cc2d923b699ef91b4c81d30964f
Nagaev Evgeny <evn1054@gmail.com>
2024-07-21 17:47:19 +0300

    sprint_2 run stylelint fix

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@import "src/styles/variables.scss";',
+        additionalData: '@import "src/styles/variables.scss.scss";',
       },
     },
   },
